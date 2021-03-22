5c7c2dfc30bec6116bb5b75cefdc38c240d8e1a4
Yasin İPEK <yasinipek@yasinipek.com>
2021-03-22 20:06:35 +0300

    Proje dosyası ekle.

---
+++ TCkimlik/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tckimlik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string tcKimlikNo = txtTc.Text;
+            bool returnvalue = false;
+            if (tcKimlikNo.Length == 11)
+            {
+                Int64 ATCNO, BTCNO, TcNo;
+                long C1, C2, C3, C4, C5, C6, C7, C8, C9, Q1, Q2;
+
+                TcNo = Int64.Parse(tcKimlikNo);
+
+                ATCNO = TcNo / 100;
+                BTCNO = TcNo / 100;
+
+                C1 = ATCNO % 10; ATCNO = ATCNO / 10;
+                C2 = ATCNO % 10; ATCNO = ATCNO / 10;
+                C3 = ATCNO % 10; ATCNO = ATCNO / 10;
+                C4 = ATCNO % 10; ATCNO = ATCNO / 10;
+                C5 = ATCNO % 10; ATCNO = ATCNO / 10;
+                C6 = ATCNO % 10; ATCNO = ATCNO / 10;
+                C7 = ATCNO % 10; ATCNO = ATCNO / 10;
+                C8 = ATCNO % 10; ATCNO = ATCNO / 10;
+                C9 = ATCNO % 10; ATCNO = ATCNO / 10;
+                Q1 = ((10 - ((((C1 + C3 + C5 + C7 + C9) * 3) + (C2 + C4 + C6 + C8)) % 10)) % 10);
+                Q2 = ((10 - (((((C2 + C4 + C6 + C8) + Q1) * 3) + (C1 + C3 + C5 + C7 + C9)) % 10)) % 10);
+
+                returnvalue = ((BTCNO * 100) + (Q1 * 10) + Q2 == TcNo);
+            }
+
+
+            if (returnvalue)
+                textBlock.Text = "TC Kimlik Numarası Geçerli";
+            else
+                textBlock.Text = "TC Kimlik Numarası Geçersiz";
+        }
+    }
+}
